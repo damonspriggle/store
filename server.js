7bde20286c5af7dd1f2b8c2ad5aec9cb43ef1e85
Damon <damonspriggle@hotmail.com>
2022-05-29 19:07:18 -0500

    finished

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const sequelize = require('./config/connection');
 // Sets up the Express App
 // =============================================================
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 
 // Sets up the Express app to handle data parsing
 app.use(express.json());
